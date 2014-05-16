850cd6b95babf527bdcee731552c5940099f5fb7
animeshinvici <mishra.animesh.2010@gmail.com>
2014-05-16 18:51:28 +0530

    adding for Diamond Operator Examples

---
+++ src/com/java7/diamondoperator/DiamondOperatorDemo.java
@@ -0,0 +1,19 @@
+package com.java7.diamondoperator;
+
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class DiamondOperatorDemo {
+	
+	public void diamondOperImple(){
+		Map<String, List<Trade>> trades = new TreeMap <> ();
+
+		
+	}
+	
+	class Trade{
+		
+	}
+
+}
